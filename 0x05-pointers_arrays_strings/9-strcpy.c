675eb9397931ff90a5f2c5de18a9bdb85d5a4345
aabduul <mustaaabdul@gmail.com>
2023-02-27 18:02:25 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,16 @@
 char *_strcpy(char *dest, char *src)
 {
 	int l = 0;
+	int n = 0;
 
 	while (*(src + l) != '\0')
 	{
-		*(dest + l) = (*src + l);
 		l++;
 	}
-	*(dest + l) = '\0';
+	for ( ; n < l ; n++)
+	{
+		dest[n] = src[n];
+	}
+	dest[l] = '\0';
 	return (dest);
 }
